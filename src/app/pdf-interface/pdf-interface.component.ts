495c7d0321df39fd816069b58bd6195a6f4a3781
dudushy <eduardotalarico1337@gmail.com>
2022-09-29 20:25:36 -0300

    Add dates

--- src/app/pdf-interface/pdf-interface.component.ts
+++ src/app/pdf-interface/pdf-interface.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
@@ -24,6 +25,7 @@ export class PdfInterfaceComponent implements OnInit {
     console.log(`[${this.title}#ngOnInit]`);
 
     this.loadImage();
+    this.setupDate();
   }
 
   ngAfterViewInit() {
@@ -33,6 +35,43 @@ export class PdfInterfaceComponent implements OnInit {
       console.log(`[${this.title}#onresize]`);
       this.updateMaskSize();
     };
+  }
+
+  setupDate() {
+    console.log(`[${this.title}#setupDate]`);
+
+    const date = new Date();
+    console.log(`[${this.title}#setupDate] date`, date);
+
+    const currentHour = date.getHours() > 10 ? date.getHours() : ('0' + date.getHours());
+    const currentMinute = date.getMinutes() > 10 ? date.getMinutes() : ('0' + date.getMinutes());
+    const currentTime = `${currentHour}:${currentMinute}`;
+    console.log(`[${this.title}#setupDate] currentTime`, currentTime);
+
+    const dateTime = document.getElementById('date-time');
+    console.log(`[${this.title}#setupDate] dateTime`, dateTime);
+    dateTime!.innerHTML = currentTime;
+
+    const currentDay = date.getDate() > 10 ? date.getDate() : ('0' + date.getDate());
+    console.log(`[${this.title}#setupDate] currentDay`, currentDay);
+
+    const dateDay = document.getElementById('date-day');
+    console.log(`[${this.title}#setupDate] dateDay`, dateDay);
+    dateDay!.innerHTML = currentDay.toString();
+
+    const currentMonth = (date.getMonth() + 1) > 10 ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1));
+    console.log(`[${this.title}#setupDate] currentMonth`, currentMonth);
+
+    const dateMonth = document.getElementById('date-month');
+    console.log(`[${this.title}#setupDate] dateMonth`, dateMonth);
+    dateMonth!.innerHTML = currentMonth.toString();
+
+    const currentYear = date.getFullYear().toString().slice(-2);
+    console.log(`[${this.title}#setupDate] currentYear`, currentYear);
+
+    const dateYear = document.getElementById('date-year');
+    console.log(`[${this.title}#setupDate] dateYear`, dateYear);
+    dateYear!.innerHTML = currentYear;
   }
 
   updateItem(event: any) {
